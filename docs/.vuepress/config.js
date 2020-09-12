f6abe78a363c9f3f890c695c451f3667feabc211
曾声亮 <zengsl@zengsl-MacBook-Pro.local>
2020-09-12 12:32:33 +0800

    增加.ico

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,9 @@ const navConf = require('./config/navConf.js');
 module.exports = {
 	title: '泥瓦匠', // 设置网站标题
 	description : '泥瓦匠',
+	head: [
+	    ['link', { rel: 'icon', href: '/favicon.ico' }]
+	  ],
 	themeConfig: {
 		logo: '/img/hero.png',
 		// sidebar: 'auto',
